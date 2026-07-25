2f474f223e769ed0295bd828d59b662b34bd2d2e
Jason Tang <jasontangoc@gmail.com>
2026-07-25 15:52:12 -0700

    save efficient frontier plot to images folder

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+import os
+
 from data_retrieval import *
 from monte_carlo import *
 from optimizer import *
 
 def main():
     df = DataSet()
-    # fig, portfolio_data = run_monte_carlo(df.tickers, df.return_vector, df.covariance_matrix)
-    # fig.show()
+
+    fig, portfolio_data = run_monte_carlo(df.tickers, df.return_vector, df.covariance_matrix)
+    os.makedirs("images", exist_ok=True)
+    fig.write_image("images/efficient_frontier.png", width=900, height=600)
 
     weights = optimize_portfolio(df)
     for ticker, weight in zip(df.tickers, weights):
         print(f"{ticker}: {weight:.2%}")
-    print(sum(weights))
 
 if __name__ == "__main__":
     main()
-    
